ea0d1d3566e7708a52b65c908c512660f8cb6a01
David Chen <ddwchensf@gmail.com>
2017-12-08 07:38:35 +0000

    Style

--- arraysepi_test.py
+++ arraysepi_test.py
@@ -1,10 +1,12 @@
 import arraysepi
 import unittest
 
+
 class TestArraysEpi(unittest.TestCase):
     def test_sumsum(self):
-        nums = [1,2,3]
+        nums = [1, 2, 3]
         self.assertEqual(arraysepi.mysum(nums), sum(nums))
-    
+
+
 if __name__ == '__main__':
     unittest.main()
