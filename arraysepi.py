2d7924ae2d20e9cd8bf26e033799114dd62c05e8
warshmellow <ddwchensf@gmail.com>
2017-12-09 08:57:10 -0800

    Add dutch

--- arraysepi.py
+++ arraysepi.py
@@ -1,5 +1,25 @@
 def mysum(nums):
     result = 0
-    for n in nums:
-        result += n
+    for num in nums:
+        result += num
     return result
+
+
+def dutch(pivot_index, nums):
+    pivot = nums[pivot_index]
+
+    smaller = 0
+    for i, _ in enumerate(nums):
+        if nums[i] < pivot:
+            nums[i], nums[smaller] = nums[smaller], nums[i]
+            smaller += 1
+
+    larger = len(nums) - 1
+    for i in reversed(range(len(nums))):
+        if nums[i] < pivot:
+            break
+        elif nums[i] > pivot:
+            nums[i], nums[larger] = nums[larger], nums[i]
+            larger -= 1
+
+    return nums
--- arraysepi_test.py
+++ arraysepi_test.py
@@ -1,11 +1,20 @@
-import arraysepi
 import unittest
+from ddt import ddt, data, unpack
+
+import arraysepi
 
 
+@ddt
 class TestArraysEpi(unittest.TestCase):
     def test_sumsum(self):
         nums = [1, 2, 3]
         self.assertEqual(arraysepi.mysum(nums), sum(nums))
+
+    @data((3, [0, 1, 2, 0, 2, 1, 1], [0, 0, 1, 2, 2, 1, 1]))
+    @unpack
+    def test_dutch(self, idx, nums, expected):
+        result = arraysepi.dutch(idx, nums)
+        self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
