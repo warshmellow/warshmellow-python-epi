a3b48f96c677b4c2b1a4e8ef05e8b39a81c9e8c7
warshmellow <ddwchensf@gmail.com>
2018-01-08 22:38:56 -0800

    Use Hypothesis for Property Based Testing

--- arraysepi_test.py
+++ arraysepi_test.py
@@ -1,12 +1,36 @@
 import unittest
+from typing import List
+
+import hypothesis.strategies as st
+from hypothesis import given
 
 import arraysepi
 
 
 class TestArraysEpi(unittest.TestCase):
-    def test_dutch(self):
-        idx = 3
-        nums = [0, 1, 2, 0, 2, 1, 1]
-        expected = [0, 0, 1, 2, 2, 1, 1]
+
+    @given(st.lists(
+        elements=st.integers(),
+        min_size=1,
+        unique=True
+    ))
+    def test_dutch(self, nums: List[int]):
+        idx = len(nums) // 2
+        dist = nums[idx]
+
         result = arraysepi.dutch(idx, nums)
-        self.assertEqual(result, expected)
+        new_idx = result.index(dist)
+
+        all_lt = True
+        for _, num in enumerate(nums[:new_idx]):
+            if num >= dist:
+                all_lt = False
+                break
+
+        all_gt = True
+        for _, num in enumerate(nums[new_idx + 1:]):
+            if num <= dist:
+                all_gt = False
+                break
+
+        self.assertTrue(all_lt and all_gt)
